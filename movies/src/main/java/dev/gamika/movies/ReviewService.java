de95a77ac8dcfb78f94b585c189731bfa33b4ec2
Gamika Jayawardana <gamikakj@gmail.com>
2025-09-12 20:40:29 +0530

    Update ReviewService.java

--- movies/src/main/java/dev/gamika/movies/ReviewService.java
+++ movies/src/main/java/dev/gamika/movies/ReviewService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
-    public Review createReview(){
-        
+    public Review createReview(String reviewBody,String imdbId){
+        Review review = new Review();
     }
 }
